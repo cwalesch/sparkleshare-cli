bc05d9c80c04beac64f6a6f1848b252d840ed033
Christoph Walesch <christoph.walesch@spaceteams.de>
2021-04-10 17:53:50 +0200

    add dazzle to vm

--- test-env/vm/vm.nix
+++ test-env/vm/vm.nix
@@ -1,12 +1,12 @@
 {
-  # Make sure that we still have admin access to the machine
+  environment.systemPackages = [
+    (import ./dazzle.nix)
+  ];
+
   services.openssh.enable = true;
   networking.firewall.allowedTCPPorts = [ 22 ];
   users = {
     mutableUsers = false;
     users.root.openssh.authorizedKeys.keyFiles = [ ./vm-ssh-key.pub ];
   };
-
-  # Rescue option
-  # users.users.root.initialPassword = "root";
 }
